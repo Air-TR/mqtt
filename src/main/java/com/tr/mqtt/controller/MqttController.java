9ba147b010a325cf112bdf88927fa04bee49c1fc
taorun <tr1838@163.com>
2023-01-31 13:45:45 +0800

    1

--- src/main/java/com/tr/mqtt/controller/MqttController.java
+++ src/main/java/com/tr/mqtt/controller/MqttController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/mqtt")
 public class MqttController {
 
     @Value("${spring.mqtt.consumerClientId}")
